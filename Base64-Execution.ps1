19c86211daafac8adfbb019816e5aac9136b96db
cparmn <parman.casey@gmail.com>
2018-01-18 18:08:58 -0600

    Add files via upload

---
+++ Base64-Execution.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cparmn/Base64/master/Program.txt" -OutVariable $
+$Program_bytes = [System.Convert]::FromBase64String(($R.ToString() -replace '\?', '' -replace '\.', ''))
+[Reflection.Assembly]::Load($Program_bytes)
+[Program]::Main()
---
+++ Base64-Generate.ps1
@@ -0,0 +1,15 @@
+﻿Add-Type -TypeDefinition @'
+using System.Reflection;
+public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Its all about the Bytes");
+    }
+}
+'@ -OutputAssembly C:\Test\Program.exe
+
+$Program_bytes = Get-Content -Path C:\Test\Program.exe -Encoding Byte
+$Program_encoded = [System.Convert]::ToBase64String($Program_bytes)
+$Program_encoded | Out-File C:\Test\Program.txt
+
